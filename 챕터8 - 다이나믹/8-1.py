7681a81aeb8b371fcda51ff5c4ffd8eeb78f405a
Java Mon <javamon1174@gmail.com>
2020-10-20 16:20:22 +0000

    update 8-1.py

--- 챕터8 - 다이나믹/8-1.py
+++ 챕터8 - 다이나믹/8-1.py
@@ -2,6 +2,7 @@
 import sys
 
 # CHAPTER 08 다이나믹 프로그래밍 => 피보나치 수열
+# 탑 다운(재귀함수)
 d = [0] * 100
 
 d[1], d[2] = 1, 1
@@ -14,3 +15,13 @@ def fibo(x):
     return d[x]
 
 print(fibo(50))
+
+# 보텀업(반복문)
+e = [0] * 100
+
+e[1], e[2], n = 1, 1, 50
+
+for i in range(3, n+1):
+    e[i] = e[i-1] + e[i-2]
+
+print(e[n])
